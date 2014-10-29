ea3a33fc1eca48a98557bc0229bb8d342e81fc10
Ramon Gebben <ramon@ra-ge.net>
2014-10-29 19:30:39 +0100

    Small fix in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,6 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  protected
-
   def logged_in?
     unless session[:user_id]
       flash[:notice] = "You need to log in first."
